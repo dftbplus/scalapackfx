14330de37a5ca10ccb6d9031de19af875180eee9
Bálint Aradi <baradi09@gmail.com>
2014-10-21 15:25:44 +0200

    Wrappers added for psymm, phemm

--- src/pblasfx.F90
+++ src/pblasfx.F90
@@ -6,12 +6,12 @@ module pblasfx_module
   use pblas_module
   implicit none
   private
-
+  
   public :: pblasfx_psyr, pblasfx_pher
   public :: pblasfx_psyrk, pblasfx_pherk
   public :: pblasfx_psymv, pblasfx_phemv
-
-
+  public :: pblasfx_psymm, pblasfx_phemm
+  
   interface pblasfx_psyr
     module procedure pblasfx_psyr_real, pblasfx_psyr_dreal
   end interface pblasfx_psyr
@@ -19,23 +19,31 @@ module pblasfx_module
   interface pblasfx_pher
     module procedure pblasfx_pher_complex, pblasfx_pher_dcomplex
   end interface pblasfx_pher
-
+  
   interface pblasfx_psyrk
     module procedure pblasfx_psyrk_real, pblasfx_psyrk_dreal
   end interface pblasfx_psyrk
-
+  
   interface pblasfx_pherk
     module procedure pblasfx_pherk_complex, pblasfx_pherk_dcomplex
   end interface pblasfx_pherk
-
+  
   interface pblasfx_psymv
     module procedure pblasfx_psymv_real, pblasfx_psymv_dreal
   end interface pblasfx_psymv
-
+  
   interface pblasfx_phemv
     module procedure pblasfx_phemv_complex, pblasfx_phemv_dcomplex
   end interface pblasfx_phemv
-
+  
+  interface pblasfx_psymm
+    module procedure pblasfx_psymm_real, pblasfx_psymm_dreal
+  end interface pblasfx_psymm
+  
+  interface pblasfx_phemm
+    module procedure pblasfx_phemm_complex, pblasfx_phemm_dcomplex
+  end interface pblasfx_phemm
+  
 contains
 
   _subroutine_pblasfx_psyr_pher(psyr_real, real, sp, real, psyr)
@@ -52,5 +60,10 @@ contains
   _subroutine_pblasfx_psymv_phemv(psymv_dreal, real, dp, real, psymv)
   _subroutine_pblasfx_psymv_phemv(phemv_complex, complex, sp, cmplx, phemv)
   _subroutine_pblasfx_psymv_phemv(phemv_dcomplex, complex, dp, cmplx, phemv)
+
+  _subroutine_pblasfx_psymm_phemm(psymm_real, real, sp, real, psymm)
+  _subroutine_pblasfx_psymm_phemm(psymm_dreal, real, dp, real, psymm)
+  _subroutine_pblasfx_psymm_phemm(phemm_complex, complex, sp, cmplx, phemm)
+  _subroutine_pblasfx_psymm_phemm(phemm_dcomplex, complex, dp, cmplx, phemm)
   
 end module pblasfx_module
